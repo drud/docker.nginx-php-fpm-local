6930e531ee213a5c92536b23823981c3e72ddca0
Brad Bowman <beeradb@gmail.com>
2017-04-12 11:04:38 -0600

    [docker-health] Add a ~2 minute timeout to the containercheck

--- test/containercheck.sh
+++ test/containercheck.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-while true;
+exitcode=1
+for i in `seq 1 60`;
 do
     status="$(docker ps --format "{{.Status}}" --filter "name=$CONTAINER_NAME" | sed  's/.*(\(.*\)).*/\1/')"
     if [[ "$status" == "healthy" ]]
     then
+        exitcode=0
         break
     fi
     sleep 2
 done
-exit 0
+exit $exitcode
