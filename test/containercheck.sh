a422d91bc1fd9288cac555f1d504b5d9b42c6bf9
tannerjfco <tannerjfco@gmail.com>
2017-04-13 11:14:54 -0600

    add comment on sed

--- test/containercheck.sh
+++ test/containercheck.sh
@@ -2,6 +2,7 @@
 exitcode=1
 for i in `seq 1 60`;
 do
+    # status contains uptime and health in parenthesis, sed to return health
     status="$(docker ps --format "{{.Status}}" --filter "name=$CONTAINER_NAME" | sed  's/.*(\(.*\)).*/\1/')"
     if [[ "$status" == "healthy" ]]
     then
