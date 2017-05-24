f68792fe6b2d6eeab47d5c1ffafa207931f00a75
Randy Fay <randy@randyfay.com>
2017-05-24 14:28:13 -0600

    Change nginx user/group if provided

--- files/start.sh
+++ files/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Change nginx to UID/GID of the docker user
+if [ -n "$UID" ] ; then
+    usermod -u $UID nginx
+fi
+if [ -n "$GID" ] ; then
+    groupmod -g $GID nginx
+fi
+
 # Display PHP errors or not
 if [[ "$ERRORS" != "1" ]] ; then
  echo php_flag[display_errors] = off >> /etc/php/7.0/fpm/php-fpm.conf
